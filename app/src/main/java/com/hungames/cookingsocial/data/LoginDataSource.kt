7f27cd9197ce0ecb55887aed7e0a97b4543f5ac1
Hung <viet.hungdinh@yahoo.de>
2021-02-04 17:51:02 +0100

    Refactor LoginDataSource.kt

--- app/src/main/java/com/hungames/cookingsocial/data/LoginDataSource.kt
+++ app/src/main/java/com/hungames/cookingsocial/data/LoginDataSource.kt
@@ -1,6 +1,5 @@
 package com.hungames.cookingsocial.data
 
-import com.hungames.cookingsocial.data.model.RegisteredUser
 import com.hungames.cookingsocial.data.model.RegisteredUserDao
 import com.hungames.cookingsocial.data.model.UserMinimal
 import com.hungames.cookingsocial.util.TAG_LOGIN
@@ -14,19 +13,19 @@ import javax.inject.Inject
 class LoginDataSource @Inject constructor(private val userDao: RegisteredUserDao) {
 
     suspend fun login(username: String, password: String): Result<UserMinimal> {
-        try {
+        return try {
 
             Timber.tag(TAG_LOGIN).i("LoginDataSource about to get the user")
             val passwordHash = password.hashCode().toString()
             val result = userDao.getUser(username, passwordHash)
             Timber.tag(TAG_LOGIN).i("Getting result finished. If it is null, one of the things are wrong: $result")
-            return if (result != null) {
+            if (result != null) {
                 Result.Success(result)
             } else {
                 Result.Error(Exception("Password or username is/are wrong."))
             }
         } catch (e: Throwable) {
-            return Result.Error(IOException("Error logging in", e))
+            Result.Error(IOException("Error logging in", e))
         }
     }
 
